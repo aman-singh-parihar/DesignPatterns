d6f5fcfe20119b37e80621ae68eff69056d6f0bf
Aman Singh Parihar <amanparihar16@gmail.com>
2024-03-05 18:21:09 +0530

    Update Program.cs

--- BehavioralPattern/ChainOfResponsibility/src/Program.cs
+++ BehavioralPattern/ChainOfResponsibility/src/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Design
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentException();
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.WriteLine("Arguemtn Excpeiont");
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excpeiont");
+
+            }
+
+            Console.WriteLine("Hello");
+
+            IHandler Civilian = new CivilianVehicleHandler();
+            IHandler Fire = new FireVehicleHandler();
+
+            Civilian.SetNextHandler(Fire);
+            Civilian.Handle("Fire");
+        }
+    }
+}
